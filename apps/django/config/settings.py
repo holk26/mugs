9f053ff654df392beb31153260e12ca63ce09a98
Homero <homero9726@gmail.com>
2026-06-24 00:32:57 -0700

    feat: configure celery with redis broker

--- apps/django/config/settings.py
+++ apps/django/config/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'corsheaders',
     'apps.products',
     'apps.printful',
@@ -132,3 +133,11 @@ if RESEND_API_KEY:
     }
 else:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# Celery
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+CELERY_TASK_ALWAYS_EAGER = os.environ.get('CELERY_TASK_ALWAYS_EAGER', 'False').lower() in ('true', '1', 'yes')
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
