f9baf3b55d830609efc79d1865ab6490050e3ece
Homero <homero9726@gmail.com>
2026-07-12 15:11:47 -0700

    Medium 3: Move apply_discount idempotency check inside locked transaction

--- apps/django/apps/discounts/views.py
+++ apps/django/apps/discounts/views.py
@@ -153,6 +153,15 @@ def apply_discount(request):
 
         order_total = sum(line.price * line.quantity for line in order.lines.all())
 
+        if order.discount_code_id == discount.id and order.discount_amount > 0:
+            return Response({
+                'discount_code': discount.code,
+                'discount_type': discount.discount_type,
+                'value': str(discount.value),
+                'discount_amount': str(order.discount_amount),
+                'order_total': str(order.total),
+            })
+
         discount = DiscountCode.objects.select_for_update().get(pk=discount.pk)
         is_valid, message = discount.is_valid(
             order_total=order_total,
